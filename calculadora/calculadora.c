6105adf3f33bef09dd11e1c3739f57ddcaece13b
Tainan <tainanccba07@gmail.com>
2021-05-17 22:31:04 -0300

    calculadora

---
+++ calculadora/calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, resultado;
+
+    printf("valor :");
+    scanf("%d", &a);
+
+
+    printf("\nvalor :");
+    scanf("%d", &b);
+
+    resultado = a + b;
+    printf("\n%d", resultado);
+    return 0;
+}
