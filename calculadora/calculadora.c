46ed35a3af6feeb629113f61fee8febfa8a06b11
Tainan <tainanccba07@gmail.com>
2021-05-17 23:23:30 -0300

    calculadora edao

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -3,16 +3,22 @@
 
 int main()
 {
-    int a, b, resultado;
+    const int limite = 5;
+    int valor[limite], i, soma = 0;
 
-    printf("valor :");
-    scanf("%d", &a);
+    for (i = 0; i < limite; i++){
+        printf ("\ndigite o valor: ");
+        scanf (" %d", &valor[i]);
+        soma += valor[i];
+    }
+    printf ("a soma de todos os numeros e: %d\n", soma);
+
+    soma = valor[0];
+    for ( i = 1; i < limite; i++){
+         printf("\n %d + %d = %d", soma, valor[i], soma + valor[i]);
+         soma += valor[i];
+    }
 
 
-    printf("\nvalor :");
-    scanf("%d", &b);
-
-    resultado = a + b;
-    printf("\n%d", resultado);
     return 0;
 }
